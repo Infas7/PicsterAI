ed51ea55381efdb9517ac4d0af2872b64c0d8a1d
Infas7 <infasim1997@gmail.com>
2024-06-05 15:03:50 +0530

    modified index.js

--- server/index.js
+++ server/index.js
@@ -37,7 +37,7 @@ app.use((req, res, next) => {
 
 //routes
 app.use("/", (req, res) => {
-  return "Hello. It's working...";
+  return res.send("Hello. It's working...");
 });
 app.use("/auth", authRouter);
 app.use("/image", requireAuth, imageRouter);
